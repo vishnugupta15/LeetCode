fc25ec654bf6e4f774f19631c85dee199c43ec59
Vishnu Gupta <96490514+vishnugupta15@users.noreply.github.com>
2026-02-23 20:34:04 +0530

    Time: 1378 ms (5.06%) | Memory: 724.6 MB (5.07%) - LeetSync

---
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void fun(unordered_set<string> &st,int l,string t,int k){
+        if(l==k){
+            st.insert(t);
+            return;
+        }
+        string t1=t+'0';
+        string t2=t+'1';
+        fun(st,l+1,t1,k);
+        fun(st,l+1,t2,k);
+    }
+    bool hasAllCodes(string s, int k) {
+        unordered_set<string> st;
+        int l=0;
+        int n=s.size();
+        if(n<k+pow(2,k)-1) return false;
+        string t="";
+        string temp="";
+        fun(st,l,t,k);
+        for(int i=0;i<k && i<n ;i++) temp+=s[i];
+
+        if(st.find(temp)!=st.end()) st.erase(temp);
+        
+        for(int i=k;i<n;i++){
+            temp.erase(0,1);
+            temp+=s[i];
+            if(st.find(temp)!=st.end()) st.erase(temp);
+            if(st.size()==0) return true;
+        }
+        return false;
+    }
+};
