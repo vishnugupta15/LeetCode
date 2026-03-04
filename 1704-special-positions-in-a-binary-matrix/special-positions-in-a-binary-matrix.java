d4939e0b367bbd2d373875b1aff72777aa26d546
Vishnu Gupta <96490514+vishnugupta15@users.noreply.github.com>
2026-03-04 08:09:07 +0530

    Time: 3 ms (28.65%) | Memory: 47 MB (91.99%) - LeetSync

---
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int [] row = new int[mat.length];
+        int [] col = new int[mat[0].length];
+        int cnt = 0;
+
+        for(int i = 0; i<mat.length; i++){
+            for(int j = 0; j<mat[0].length; j++){
+                if(mat[i][j]==1){
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+        for(int i = 0; i<mat.length; i++){
+            for(int j = 0; j<mat[0].length; j++){
+                if(mat[i][j]==1 && row[i]==1 && col[j]==1)cnt++;
+            }
+        }
+        return cnt;
+    }
+}
