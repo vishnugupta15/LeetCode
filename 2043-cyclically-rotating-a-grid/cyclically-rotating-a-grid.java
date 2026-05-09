71dac75de27fd189537a28eddba3946780f89d2d
Vishnu Gupta <96490514+vishnugupta15@users.noreply.github.com>
2026-05-09 18:04:12 +0530

    Time: 6 ms (44.78%) | Memory: 47.5 MB (40.30%) - LeetSync

---
+++ 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int[][] rotateGrid(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int layers = Math.min(m,n) / 2;
+
+        for(int layer = 0; layer < layers; layer++){
+            List<Integer> nums = new ArrayList<>();
+
+            int top = layer;
+            int bottom = m-layer-1;
+            int left = layer;
+            int right = n-layer-1;
+
+            for(int j = left; j<=right; j++){
+                nums.add(grid[top][j]);
+            }
+
+            for(int j = top+1; j<= bottom-1; j++){
+                nums.add(grid[j][right]);
+            }
+
+            for(int j = right; j>=left; j--){
+                nums.add(grid[bottom][j]);
+            }
+
+            for(int j = bottom-1; j>=top+1; j--){
+                nums.add(grid[j][left]);
+            }
+
+            int len = nums.size();
+
+            int normalizedK = k % len;
+
+            Collections.rotate(nums, -normalizedK);
+
+            int idx = 0;
+
+            for(int j = left; j<=right; j++){
+                grid[top][j] = nums.get(idx++);
+            }
+
+            for(int j = top+1; j<= bottom-1; j++){
+                grid[j][right] = nums.get(idx++);
+            }
+
+            for(int j = right; j>=left; j--){
+                grid[bottom][j] = nums.get(idx++);
+            }
+
+            for(int j = bottom-1; j>=top+1; j--){
+                grid[j][left] = nums.get(idx++);
+            }
+        }
+        return grid;
+    }
+}
